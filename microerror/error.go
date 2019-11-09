711a22d3a909e2d1071be2c5caeac1326da35c3f
zuoxiupeng <zuoxiupeng@live.com>
2019-11-09 23:01:41 +0800

    错误对象和grpc关系更密切

--- microerror/error.go
+++ microerror/error.go
@@ -3,22 +3,24 @@ package microerror
 import (
 	"encoding/json"
 
-	"github.com/jinzhu/gorm"
-	goredis "gopkg.in/redis.v5"
+	"google.golang.org/grpc/status"
 )
 
 /* 微服务返回错误代码列表 */
 
 // TODO 此处错误代码应在某区间内为公共错误码、各自程序可以在init中将错误码补充到错误列表
 
-// 基础错误 - 错误码 +-32768
-var (
-	errors = map[int32]*MicroError{
-		10000: NewMicroError(10000, "Unknown server error", nil), // 服务端错误
+const (
+	// UnknownServerError 服务端默认错误
+	UnknownServerError uint32 = 10000
+)
 
-		10001: NewMicroError(10001, "record not found", gorm.ErrRecordNotFound), // db数据未查询到
-		10101: NewMicroError(10101, "key not found", goredis.Nil),               // redis key为空
-		10102: NewMicroError(10102, "Redis operation error", nil),               // redis 操作错误
+// 基础错误 - 错误码为int32数字
+var (
+	errors = map[uint32]*MicroError{
+		/* 常用基础错误 */
+		10000: NewMicroError(10000, "Unknown server error"), // 服务端错误
+		10001: NewMicroError(10001, "record not found"),     // db数据未查询到
 
 	}
 )
@@ -26,16 +28,14 @@ var (
 // MicroError 错误类型
 type MicroError struct {
 	Msg  string `json:"msg"`  // 错误信息
-	Code int32  `json:"code"` // 错误代码
-	Err  error  `json:"err"`  // 原始err信息
+	Code uint32 `json:"code"` // 错误代码
 }
 
 // NewMicroError 创建MicroError
-func NewMicroError(code int32, msg string, err error) *MicroError {
+func NewMicroError(code uint32, msg string) *MicroError {
 	return &MicroError{
 		Code: code,
 		Msg:  msg,
-		Err:  err,
 	}
 }
 
@@ -46,20 +46,21 @@ func (err *MicroError) Error() string {
 }
 
 // GetMicroError 通过MicroError创建错误，补充err部分
-func GetMicroError(code int32, errs ...error) *MicroError {
+func GetMicroError(code uint32, errs ...error) *MicroError {
 	var err error
+	msg := ""
 	if len(errs) > 0 {
 		err = errs[0]
+		if err != nil {
+			msg = err.Error()
+		}
 	}
 	microErr := &MicroError{
 		Code: code,
-		Err:  err,
+		Msg:  msg,
 	}
 	if mErr, ok := errors[code]; ok == true {
 		microErr.Msg = mErr.Msg
-		if err == nil {
-			microErr.Err = mErr.Err
-		}
 	} else {
 		microErr.Msg = errors[10000].Msg
 	}
@@ -70,5 +71,17 @@ func GetMicroError(code int32, errs ...error) *MicroError {
 func InitError(errs ...*MicroError) {
 	for _, err := range errs {
 		errors[err.Code] = err
+	}
+}
+
+// Convert 解析错误信息 - 统一客户端创建时返回此错误
+func Convert(err error) *MicroError {
+	if err == nil {
+		return nil
+	}
+	grpcStatus := status.Convert(err)
+	return &MicroError{
+		Code: uint32(grpcStatus.Code()),
+		Msg:  grpcStatus.Message(),
 	}
 }
